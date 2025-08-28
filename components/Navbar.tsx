3738751f7e05c08018f88bd8e00787d8fca23cd5
qaspercreates <qasper890@gmail.com>
2025-08-28 22:38:45 +0100

    Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,1 +1,12 @@
-
+export default function Navbar() {
+  return (
+    <header className="border-b border-zinc-800">
+      <div className="container py-3 flex items-center justify-between">
+        <a href="/" className="flex items-center gap-2 no-underline">
+          <img src="/logo.svg" alt="OurSpace" className="h-6 w-auto" />
+        </a>
+        <a href="/feed" className="btn-outline">Enter the Feed</a>
+      </div>
+    </header>
+  );
+}
