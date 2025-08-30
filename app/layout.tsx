4240193c706452f11be743eae3dfbd65b8c21247
qaspercreates <qasper890@gmail.com>
2025-08-30 21:00:47 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,15 @@
+import "./globals.css";
+import React from "react";
+
 export const metadata = {
   title: "OurSpace",
   description: "The internet, unfiltered."
 };
 
-import "./globals.css";
-import React from "react";
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="min-h-screen">{children}</body>
     </html>
   );
 }
