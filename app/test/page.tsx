300c34b41cf8465f88611d2261a8df0299711467
qaspercreates <qasper890@gmail.com>
2025-08-31 17:36:10 +0100

    Create page.tsx

---
+++ app/test/page.tsx
@@ -0,0 +1,9 @@
+export default function Test() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="rounded-xl p-6 text-white text-xl bg-blue-600 shadow-lg">
+        Tailwind is working ✅
+      </div>
+    </div>
+  );
+}
