a0c94c4fdca44cdb294b7ac811e885dc5e3de5e7
qaspercreates <qasper890@gmail.com>
2025-09-01 15:02:35 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  typescript: {
+    // build should not stop on type errors
+    ignoreBuildErrors: true
+  }
 };
 
 export default nextConfig;
