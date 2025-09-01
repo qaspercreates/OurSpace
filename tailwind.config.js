9857d2fefd82784fb0c3509c7baeaa0bfc4ec52c
qaspercreates <qasper890@gmail.com>
2025-09-01 15:32:20 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./styles/**/*.css" // <- correct glob
+    "./styles/**/*.css"
   ],
   theme: { extend: {} },
   plugins: []
